cf8676e72eafa46e81842b04d00711bcca4451e9
YestoryFornever <guosj90@163.com>
2016-12-21 14:05:20 +0800

    还原误删

--- client/app/app.js
+++ client/app/app.js
@@ -6,5 +6,14 @@ import AppComponent from './app.component';
 import 'normalize.css';
 
 angular.module('app', [
-
-  .component('app', AppComponent);
+	uiRouter,
+		Common,
+		Components
+	])
+	.config(($locationProvider) => {
+		"ngInject";
+		// @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
+		// #how-to-configure-your-server-to-work-with-html5mode
+		$locationProvider.html5Mode(true).hashPrefix('!');
+	})
+	.component('app', AppComponent);
