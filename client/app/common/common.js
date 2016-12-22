89e4e030b8ddd822cb1a1747586b2abe29fde6c1
YestoryFornever <guosj90@163.com>
2016-12-22 10:07:39 +0800

    #删除无用模块#

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,7 +1,4 @@
 import angular from 'angular';
-import Navbar from './navbar/navbar';
-import Hero from './hero/hero';
-import User from './user/user';
 import PageHeader from './pageheader/pageheader';
 import PageNav from './pagenav/pagenav';
 import PageTab from './pagetab/pagetab';
@@ -9,9 +6,6 @@ import PagePanel from './pagepanel/pagepanel';
 import PageFooter from './pagefooter/pagefooter';
 
 let commonModule = angular.module('app.common', [
-	Navbar,
-	Hero,
-	User,
 	PageHeader,
 	PageNav,
 	PageTab,
