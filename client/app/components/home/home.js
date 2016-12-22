cb307d11b4b9d5683689e2adf5f9101b80f53958
YestoryFornever <guosj90@163.com>
2016-12-22 14:57:05 +0800

    初级路由

--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -1,6 +1,9 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import homeComponent from './home.component';
+import pageHeader from '../../common/pageheader/pageheader.component';
+import pagePanel from '../../common/pagepanel/pagepanel.component';
+import pageFooter from '../../common/pagefooter/pagefooter.component';
 
 let homeModule = angular.module('home', [
 	uiRouter,
@@ -14,11 +17,43 @@ let homeModule = angular.module('home', [
 	$stateProvider
 		.state('home', {
 			url: '/',
-			component: 'home'
+			views: {
+				'': {
+					component: 'home'
+				},
+				'header@home': {
+					component: 'header'
+				},
+				'main@home': {
+					component: 'main'
+				},
+				'footer@home': {
+					component: 'footer'
+				}
+			}
+		})
+		.state('home.a1', {
+			url: 'a1',
+			views: {
+				'main@home': {
+					component: 'footer'
+				}
+			}
+		})
+		.state('home.a2', {
+			url: 'a2',
+			views: {
+				'main@home': {
+					component: 'header'
+				}
+			}
 		});
 })
 
 .component('home', homeComponent)
+.component('header',pageHeader)
+.component('main',pagePanel)
+.component('footer',pageFooter)
 
 .name;
 
