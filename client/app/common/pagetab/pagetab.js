44980ec919d4dbf02aed40f14dc35ff4aad34cf2
YestoryFornever <guosj90@163.com>
2016-12-23 15:18:46 +0800

    导航样式

--- client/app/common/pagetab/pagetab.js
+++ client/app/common/pagetab/pagetab.js
@@ -1,3 +1,5 @@
+import $ from '../../../jquery-vendor.js';
+import bootstrap from 'bootstrap';
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import pagetabComponent from './pagetab.component';
