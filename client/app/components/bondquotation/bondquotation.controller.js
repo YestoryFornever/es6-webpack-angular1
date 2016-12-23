c50e0500d3490f76e616c6404fa6ce961426b3ea
YestoryFornever <guosj90@163.com>
2016-12-23 15:19:27 +0800

    抄袭phonecat

--- client/app/components/bondquotation/bondquotation.controller.js
+++ client/app/components/bondquotation/bondquotation.controller.js
@@ -1,7 +1,22 @@
 class BondquotationController {
-	constructor() {
+	constructor($http) {
 		this.name = 'bondquotation';
+		this.$http = $http;
+		console.log($http);
+		this.phones = [
+			{
+				name: '茶花女',
+				snippet: '小仲马'
+			}, {
+				name: '羊脂球',
+				snippet: '莫泊桑'
+			}, {
+				name: '杜十娘',
+				snippet: '不知道'
+			}
+		];
+		this.orderProp = 'name';
 	}
 }
-
+BondquotationController.$inject = ['$http'];
 export default BondquotationController;
