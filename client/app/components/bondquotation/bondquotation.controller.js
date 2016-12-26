09310aec921c83278240a623cc689b98ebe14247
YestoryFornever <guosj90@163.com>
2016-12-26 14:42:23 +0800

    定时器测试

--- client/app/components/bondquotation/bondquotation.controller.js
+++ client/app/components/bondquotation/bondquotation.controller.js
@@ -1,31 +1,41 @@
 class BondquotationController {
-	constructor($http,plus) {
+	constructor($http, $timeout, plus) {
 		this.name = 'bondquotation';
+		this.orderProp = 'name';
 		this.$http = $http;
-		console.log($http);
+		this.$timeout = $timeout;
+		this.plus = plus;
 		this.phones = [
 			{
 				name: '茶花女',
 				snippet: '小仲马',
 				check:true,
 				no:1,
-			}, {
+			},
+			{
 				name: '羊脂球',
 				snippet: '莫泊桑',
 				check:false,
 				no:2,
-			}, {
+			},
+			{
 				name: '杜十娘',
 				snippet: '不知道',
 				check:false,
 				no:3,
 			}
 		];
-		this.orderProp = 'name';
-		this.minus = function(no){
-			return plus.minus(++no);
-		}
-	}
+		this.updateClock();
+	};
+	minus(no){
+		return this.plus.minus(++no);
+	};
+	updateClock(){
+		this.clock = new Date();
+		this.$timeout(()=>{
+			this.updateClock();
+		},1000);
+	};
 }
-BondquotationController.$inject = ['$http','plusService'];
+BondquotationController.$inject = ['$http','$timeout','plusService'];
 export default BondquotationController;
