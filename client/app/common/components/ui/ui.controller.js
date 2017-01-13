36393599e53b38e94d6b535c7ec9a89760a2da6e
YestoryFornever <guosj90@163.com>
2017-01-13 14:05:33 +0800

    布局+列表

--- client/app/common/components/ui/ui.controller.js
+++ client/app/common/components/ui/ui.controller.js
@@ -20,17 +20,17 @@ class UiController {
 		var myChart = echarts.init(document.getElementById('main'));
 		// 绘制图表
 		myChart.setOption({
-		    title: { text: '结算行情' },
-		    tooltip: {},
-		    xAxis: {
-		        data: ["债券A","债券Z","债券G","债券H","债券J","债券S"]
-		    },
-		    yAxis: {},
-		    series: [{
-		        name: '销量',
-		        type: 'bar',
-		        data: [5, 20, 36, 10, 10, 20]
-		    }]
+			title: { text: '结算行情' },
+			tooltip: {},
+			xAxis: {
+				data: ["债券A","债券Z","债券G","债券H","债券J","债券S"]
+			},
+			yAxis: {},
+			series: [{
+				name: '销量',
+				type: 'bar',
+				data: [5, 20, 36, 10, 10, 20]
+			}]
 		});
 	}
 	showGridBottomSheet() {
