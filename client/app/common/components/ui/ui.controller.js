67d9f545d805a2012fa41517891e258824479271
YestoryFornever <guosj90@163.com>
2017-01-13 14:11:11 +0800

    ui 删除柱状图

--- client/app/common/components/ui/ui.controller.js
+++ client/app/common/components/ui/ui.controller.js
@@ -16,22 +16,6 @@ class UiController {
 		this.noCache = true;
 		this.isDisabled = false;
 		this.states = this.loadAll();
-		// 基于准备好的dom，初始化echarts实例
-		var myChart = echarts.init(document.getElementById('main'));
-		// 绘制图表
-		myChart.setOption({
-			title: { text: '结算行情' },
-			tooltip: {},
-			xAxis: {
-				data: ["债券A","债券Z","债券G","债券H","债券J","债券S"]
-			},
-			yAxis: {},
-			series: [{
-				name: '销量',
-				type: 'bar',
-				data: [5, 20, 36, 10, 10, 20]
-			}]
-		});
 	}
 	showGridBottomSheet() {
 		this.alert = '';
